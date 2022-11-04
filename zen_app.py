71c4e5acf2a61dba0759eec90029a95850dff72a
arek <jak47l.arekkn@gmail.com>
2022-11-04 15:41:10 +0100

    commit n

--- zen_app.py
+++ zen_app.py
@@ -2,14 +2,17 @@ import streamlit as st
 import altair as alt
 import pandas as pd
 import numpy as np
-import os, urllib
-import is_cat 
+import os
+import front.is_cat as is_cat 
 from PIL import Image
 @st.cache
 def load_image(img_file):
     img=Image.open(img_file)
     return img
-
+@st.cache
+def find_similar(img_file):
+    files=[Image.open(f) for f in os.listdir("Korat")]
+    return files
 def main():
 
     st.markdown("# Welcome to Find your soul cat!")
@@ -18,6 +21,10 @@ def main():
         st.image(load_image(img_file))
         st.write(is_cat.is_cat(load_image(img_file)))
         st.write(is_cat.what_is(load_image(img_file)))
+        for f in find_similar(img_file):
+            st.image(load_image(f))
+
+    st.markdown("# End of site bye bye!")
 
 if __name__ == '__main__':
     main()
