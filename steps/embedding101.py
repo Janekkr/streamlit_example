267f2dba6f6d8359e0f4d27675407a27fa59b7b6
Janekkr <72567101+Janekkr@users.noreply.github.com>
2022-11-05 20:20:01 +0100

    embeddings_pipeline_steps

---
+++ steps/embedding101.py
@@ -0,0 +1,14 @@
+from zenml.steps import step
+from PIL import Image
+import torchvision.models
+from eval import eval
+
+resnet101_weights = torchvision.models.ResNet101_Weights.DEFAULT
+resnet101_model = torchvision.models.resnet101(weights=resnet101_weights)
+
+@step()
+def save_resnet101_embeddings(img_name_list: list) -> list:
+    img_embeddings_list = []
+    for path in img_name_list:
+        img_embeddings_list.append(eval(Image.open(path), resnet101_model, resnet101_weights))
+    return img_embeddings_list
---
+++ steps/embedding18.py
@@ -0,0 +1,14 @@
+from zenml.steps import step
+from PIL import Image
+import torchvision.models
+from eval import eval
+
+resnet18_weights = torchvision.models.ResNet18_Weights.DEFAULT
+resnet18_model = torchvision.models.resnet18(weights=resnet18_weights)
+
+@step()
+def save_resnet18_embeddings(img_name_list: list) -> list:
+    img_embeddings_list = []
+    for path in img_name_list:
+        img_embeddings_list.append(eval(Image.open(path), resnet18_model, resnet18_weights))
+    return img_embeddings_list
